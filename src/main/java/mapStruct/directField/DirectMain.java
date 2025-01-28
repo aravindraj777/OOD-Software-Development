3f6e8add80c7b8ede99fd7f948809f319fc6b600
aravindraj <aravindraj.rs@afcdigitalinc.com>
2025-01-28 16:33:17 +0530

    Mapstruct: Direct field mapping

---
+++ src/main/java/mapStruct/directField/DirectMain.java
@@ -0,0 +1,35 @@
+package mapStruct.directField;
+
+import mapStruct.directField.StudentEntity;
+import mapStruct.directField.SubjectEntity;
+
+public class DirectMain {
+
+    public static void main(String[] args) {
+
+        SubjectEntity subject1 = new SubjectEntity();
+        subject1.setName("English");
+
+        mapStruct.directField.StudentEntity studentEntity = new StudentEntity();
+        studentEntity.setName("Ashish");
+        studentEntity.setId(1);
+        studentEntity.setClassVal("11B");
+        studentEntity.setSubject(subject1);
+
+        Student student = new Student();
+        student.setName("Aravind");
+        student.setClassName("9B");
+        student.setId(3);
+        student.setSubject(subject1.getName());
+
+        StudentEntity studentEntity1 = StudentMapper.INSTANCE.getEntityFromModel(student);
+        Student getStudent = StudentMapper.INSTANCE.getModelFromEntity(studentEntity1);
+
+        // Print the mapped StudentEntity object
+        System.out.println("Mapped StudentEntity: ");
+        System.out.println("ClassVal: " + studentEntity1.getClassVal());
+        System.out.println("Subject Name: " + studentEntity1.getSubject().getName());
+        System.out.println(getStudent.getClassName());
+
+    }
+}
---
+++ src/main/java/mapStruct/directField/StudentMapper.java
@@ -0,0 +1,21 @@
+package mapStruct.directField;
+
+import org.mapstruct.InheritInverseConfiguration;
+import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
+import org.mapstruct.factory.Mappers;
+@Mapper
+public interface StudentMapper {
+
+
+    StudentMapper INSTANCE = Mappers.getMapper(StudentMapper.class);
+
+
+    @Mapping(target = "className",source = "classVal")
+    @Mapping(target = "subject",source = "subject.name")
+    Student getModelFromEntity(StudentEntity studentEntity);
+
+    @InheritInverseConfiguration
+    StudentEntity getEntityFromModel(Student student);
+
+}
